a12952ac2a28e50515f71de20a43c7be12a4f734
pedromoraisf <pedro.morais1997@gmail.com>
2020-05-02 17:32:08 -0300

    feat: header

--- frontend/src/components/global/Navigation.vue
+++ frontend/src/components/global/Navigation.vue
@@ -1,17 +1,32 @@
 <template>
   <nav class="w-100 border-bottom">
-    <b-container class="py-4">
+    <b-container class="py-4 d-flex align-items-center justify-content-between">
+      <!-- logo -->
       <logo />
+
+      <!-- search component -->
+      <search />
+
+      <!-- user navigation -->
+      <vs-button
+        color="slack"
+        class="px-3 py-1 outline-none"
+      >Realizar Pedido</vs-button>
     </b-container>
   </nav>
 </template>
 
 <script>
 import Logo from "@/components/global/Logo";
+import Search from "@/components/global/Search";
 
 export default {
   components: {
-    Logo
+    Logo,
+    Search
   }
-}
+};
 </script>
+
+<style scoped>
+</style>
